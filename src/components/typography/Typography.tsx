d485613515f3648896944fa702ebd8e1e15a4d0d
Mykola Petrunin <mykola.petrunin@gmail.com>
2024-05-29 15:53:12 +0200

    add changes

--- src/components/typography/Typography.tsx
+++ src/components/typography/Typography.tsx
@@ -1,16 +1,16 @@
 import { createElement, FC, PropsWithChildren } from 'react';
 
 export const Typography: FC<
-  PropsWithChildren<{
-    component?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'div';
-    className?: string;
-  }>
+    PropsWithChildren<{
+        component?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'div';
+        className?: string;
+    }>
 > = ({ component = 'span', children }) => {
-  return createElement(
-    component,
-    {
-      className: 'text-black',
-    },
-    children,
-  );
+    return createElement(
+        component,
+        {
+            className: 'text-black'
+        },
+        children
+    );
 };
