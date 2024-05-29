6defd260306918b5772584f7a7218305e0d5debb
Mykola Petrunin <mykola.petrunin@gmail.com>
2024-05-29 15:52:14 +0200

    add typography

--- src/config/auth.js
+++ src/config/auth.js
@@ -4,53 +4,53 @@ import { sql } from '@vercel/postgres';
 import { v4 as uuidv4 } from 'uuid';
 
 export const {
-  auth,
-  handlers: { GET, POST },
+    auth,
+    handlers: { GET, POST }
 } = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    async signIn({ user, account, profile }) {
-      let users = await sql`
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            let users = await sql`
         SELECT * FROM public."User" WHERE email = ${user.email}
       `;
 
-      if (users.rows.length === 0) {
-        await sql`
+            if (users.rows.length === 0) {
+                await sql`
           INSERT INTO public."User" (id, email, name, image)
           VALUES (${uuidv4()}, ${user.email}, ${user.name}, ${user.image})
         `;
-        users = await sql`
+                users = await sql`
           SELECT * FROM public."User" WHERE email = ${user.email}
         `;
-      }
+            }
 
-      user.id = users.rows[0].id;
-      return true;
+            user.id = users.rows[0].id;
+            return true;
+        },
+        async session({ session, token }) {
+            session.user.id = token.uid;
+            return session;
+        },
+        async jwt({ token, user }) {
+            if (user) {
+                token.uid = user.id;
+            }
+            return token;
+        },
+        async authorized({ auth, request: { nextUrl } }) {
+            const isLoggedIn = !!auth?.user;
+            const isOnDashboard = !nextUrl.pathname.includes('auth');
+            if (isOnDashboard) {
+                return isLoggedIn;
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/', nextUrl));
+            }
+        }
     },
-    async session({ session, token }) {
-      session.user.id = token.uid;
-      return session;
-    },
-    async jwt({ token, user }) {
-      if (user) {
-        token.uid = user.id;
-      }
-      return token;
-    },
-    async authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = !nextUrl.pathname.includes('auth');
-      if (isOnDashboard) {
-        return isLoggedIn;
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/', nextUrl));
-      }
-    },
-  },
-  secret: process.env.AUTH_SECRET,
+    secret: process.env.AUTH_SECRET
 });
